c6f891b5a7577d273a35027545bb28bf3f3e544e
viniciuspsilvas <viniciuspsilvas@gmail.com>
2019-07-31 21:44:43 +1000

    Implemented deleteEnrollment

--- common/models/enrollment.js
+++ common/models/enrollment.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Enrollment) {
+module.exports = function (Enrollment) {
 
+    /**
+     * This method is resposable to remove the relationship between Student and StudentGroup
+     * @param {number} idStudent 
+     * @param {number} idGroup 
+     * @param {Function(Error, string)} cb
+     */
+
+    Enrollment.deleteEnrollment = function (idStudent, idGroup, cb) {
+        const filter = {
+            studentGroupId: idGroup ,
+            studentId: idStudent
+        }
+        Enrollment.destroyAll(filter, cb)
+    };
 };
